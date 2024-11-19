5a3aea63ce65791b35cbfd81027a219f784bd472
Mahelchandupa <mahelchandupa1013@gmail.com>
2024-11-19 23:40:02 +0530

    implement endpoints get all buses, get buses based on route and get bus details by id

--- controllers/bus.controller.js
+++ controllers/bus.controller.js
@@ -4,6 +4,7 @@ const { parseBody } = require("../utils/parseBody");
 const Bus = require("../models/bus.model");
 const { registerBusValidator } = require("../validators/bus.validator");
 const { generateSeatLayout } = require("../utils/generateSeatLayout");
+const mongoose = require("mongoose");
 
 const registerBus = async (req, res) => {
   try {
@@ -58,4 +59,63 @@ const registerBus = async (req, res) => {
   }
 };
 
-module.exports = { registerBus };
+// Get All Buses
+const getAllBuses = async (req, res) => {
+  try {
+    const buses = await Bus.find();
+
+    res.statusCode = 200;
+    res.end(responseHandler("Fetch All Buses", 200, buses));
+  } catch (error) {
+    res.statusCode = 500;
+    res.end(errorHandler("Internal Server Error", 500));
+  }
+};
+
+// Get Buses by Route ID
+const getBusesByRouteId = async (req, res) => {
+  try {
+    console.log('called')
+    const { routeId } = req;
+
+    const buses = await Bus.find({
+      routeId: routeId,
+    });
+
+    res.statusCode = 200;
+    res.end(responseHandler("Fetch All Buses Based Route", 200, buses));
+  } catch (error) {
+    res.statusCode = 500;
+    res.end(errorHandler("Internal Server Error", 500));
+  }
+};
+
+// Get Bus by ID
+const getBusById = async (req, res) => {
+  try {
+    const { busId } = req;
+
+    // Validate busId as a valid ObjectId
+    if (!mongoose.Types.ObjectId.isValid(busId)) {
+      res.statusCode = 400;
+      return res.end(errorHandler("Invalid Bus ID", 400));
+    }
+
+    const bus = await Bus.findOne({
+      _id: busId,
+    });
+
+    if (!bus) {
+      res.statusCode = 404;
+      res.end(errorHandler("Bus not found", 404));
+    } else {
+      res.statusCode = 200;
+      res.end(responseHandler("Fetch bus details", 200, bus));
+    }
+  } catch (error) {
+    res.statusCode = 500;
+    res.end(errorHandler("Internal Server Error", 500));
+  }
+};
+
+module.exports = { registerBus, getBusesByRouteId, getBusById, getAllBuses };
--- routes/bus.route.js
+++ routes/bus.route.js
@@ -1,4 +1,9 @@
-const { registerBus } = require("../controllers/bus.controller");
+const {
+  registerBus,
+  getAllBuses,
+  getBusById,
+  getBusesByRouteId,
+} = require("../controllers/bus.controller");
 const { ROLES } = require("../helpers/roles");
 const { verifyToken } = require("../utils/verifyUser");
 const { parse } = require("url");
@@ -9,10 +14,37 @@ const busRoutes = async (req, res) => {
   const path = parsedUrl.pathname;
   const query = querystring.parse(parsedUrl.query);
 
+  // (/api/v1/buses/:id)
+  const getBusByIdRegex = /^\/api\/v1\/buses\/(\w+)/;
+  const matchGetBusById = path.match(getBusByIdRegex);
+
+  // (/api/v1/buses/route/:id)
+  const getBusesByRouteIdRegex = /^\/api\/v1\/buses\/route\/(\w+)/;
+  const matchGetBusesByRouteId = path.match(getBusesByRouteIdRegex);
+
   if (path === "/api/v1/buses" && req.method === "POST") {
     try {
-      await verifyToken(req, res, ROLES.ADMIN); // Await for token verification
+      await verifyToken(req, res, ROLES.ADMIN);
       await registerBus(req, res);
+    } catch (error) {}
+  } else if (path === "/api/v1/buses" && req.method === "GET") {
+    try {
+      await verifyToken(req, res);
+      await getAllBuses(req, res);
+    } catch (error) {}
+  } else if (matchGetBusesByRouteId && req.method === "GET") {
+    try {
+      const routeId = matchGetBusesByRouteId[1];
+      await verifyToken(req, res);
+      req.routeId = routeId;
+      await getBusesByRouteId(req, res);
+    } catch (error) {}
+  } else if (matchGetBusById && req.method === "GET") {
+    try {
+      const busId = matchGetBusById[1];
+      await verifyToken(req, res);
+      req.busId = busId;
+      await getBusById(req, res);
     } catch (error) {}
   } else {
     return false;
