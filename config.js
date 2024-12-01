dc5b255551b2dbbaeea660a6cf54bf09d52e336d
Mahelchandupa <mahelchandupa1013@gmail.com>
2024-12-01 23:23:54 +0530

    update db config file

--- config.js
+++ config.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+mongoose.set("strictQuery", false);
 
 dotenv.config();
 
+const uri = process.env.MONGOOSE;
+
 async function connectDB() {
-  await mongoose
-    .connect(process.env.MONGOOSE)
+  await mongoose.connect(uri)
     .then(() => {
       console.log("Connected to MongoDB!");
     })
     .catch((err) => {
       console.log("Error connecting to MongoDB: ", err);
+      process.exit(1);
     });
 }
 
